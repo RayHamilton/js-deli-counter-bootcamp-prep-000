021bbf0141645eeeec62d7ba879c449bf549e280
Ray Hamilton <hamilton.ray@gmail.com>
2017-09-17 07:32:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var katzDeliLine = []
+var katzDeli = []
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  return (`Welcome, ${name}. You are number ` + katzDeliLine.length + ` in line.`)
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  return (`Welcome, ${name}. You are number ` + katzDeli.length + ` in line.`)
 }
 
 function nowServing(){
