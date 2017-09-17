47caa8087ce66bcbd195c1788a81251d8f3e23fb
Ray Hamilton <hamilton.ray@gmail.com>
2017-09-17 07:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeli, name){
 }
 
 function nowServing(){
-  if (katzDeli.length  ==[]){
+  if (katzDeli.length  === 0){
     return "There is nobody waiting to be served!"
   } else {
     return katzDeli.shift()
